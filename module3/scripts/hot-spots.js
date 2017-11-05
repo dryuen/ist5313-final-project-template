86c5dcc0547dcec2d62e79b14e351fa8893deaf7
vacosta <vacosta@valeries-air>
2017-11-05 13:52:28 -0600

    Hot Spots Update

--- module3/scripts/hot-spots.js
+++ module3/scripts/hot-spots.js
@@ -9,19 +9,27 @@ function showText( number )
 	
 	if( number == 1 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le chat";
+		document.getElementById( "output-text" ).innerHTML = "Add or subtract terms that either share the same variable and same exponent or are constants";
 	}
 	else if( number == 2 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le cheval";
+		document.getElementById( "output-text" ).innerHTML = "Multiply a term outside of a parenthesis to each term inside the parenthesis";
 	}
 	else if( number == 3 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le chien";
+		document.getElementById( "output-text" ).innerHTML = "Add the same term to both sides of an equation";
 	}
 	else if( number == 4 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "l'oiseau";
+		document.getElementById( "output-text" ).innerHTML = "Subtract the same term from both sides of an equation";
+	}
+	else if( number == 5 )
+	{
+		document.getElementById( "output-text" ).innerHTML = "Multiply the same term to both sides of an equation";
+	}
+	else if( number == 6 )
+	{
+		document.getElementById( "output-text" ).innerHTML = "Divide the same term from both sides of an equation";
 	}
 }
 
