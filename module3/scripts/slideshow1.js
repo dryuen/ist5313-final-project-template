5fe87ba2cd289626804786a32c2436a63efd1ff4
wei <tdg054@my.utsa.edu>
2017-11-08 13:44:19 -0600

    change image names

--- module3/scripts/slideshow1.js
+++ module3/scripts/slideshow1.js
@@ -1,18 +1,18 @@
-var images = ["images/slide5.jpg", "images/slide6.jpg", "images/slide7.jpg"] ;
+var images = ["images/myslide1.jpg", "images/myslide2.jpg", "images/myslide3.png"] ;
  var heading = ["Living With Less","Mentally Declutter","Slow Down Your Pace"] ;
- var caption = ["Better to donate than accumulate", " clear your mind of the obsessive thoughts,cut off the thought which cause overwhelm and keep you mired in
-                    avoidance and indecision.", "  Slowing Down is Everything to Create Mental Peace.Focus on the lifestyle, not the goal. The journey is the destination.Enjoy the journey!"];
+ var caption = ["Better to donate than accumulate", "clear your mind of the obsessive thoughts,cut off the thought which cause overwhelm and keep you mired in
+                    avoidance and indecision.", "Slowing Down is Everything to Create Mental Peace.Focus on the lifestyle, not the goal. The journey is the destination.Enjoy the journey!"];
  
- var imageNumber = 5;
+ var imageNumber = 0;
  var imageLength = images.length - 1;
  
  function changeImage(x){
 	 imageNumber += x;
 	 if( imageNumber > imageLength ) {
-		 imageNumber =5;
+		 imageNumber =0;
 	 }
 	 
-	 if (imageNumber < 5){
+	 if (imageNumber < 0){
 		 imageNumber = imageLength;
 	 } 
 	 
