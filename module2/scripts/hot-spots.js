53e080bdaf115d7974b5d0dcd92cbede88a5681e
marinajohnson <marinajohnson@10.0.1.5>
2017-11-08 17:08:53 -0600

    added hot spot scripts mod2

--- module2/scripts/hot-spots.js
+++ module2/scripts/hot-spots.js
@@ -9,19 +9,19 @@ function showText( number )
 	
 	if( number == 1 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le chat";
+		document.getElementById( "output-text" ).innerHTML = "Check out the color and clarity.";
 	}
 	else if( number == 2 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le cheval";
+		document.getElementById( "output-text" ).innerHTML = "Swirl the wine slow and easy. The more droplets of wine on the glass=higher alcohol content.";
 	}
 	else if( number == 3 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le chien";
+		document.getElementById( "output-text" ).innerHTML = "Bring the wine up to the nose and take in the aroma.";
 	}
 	else if( number == 4 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "l'oiseau";
+		document.getElementById( "output-text" ).innerHTML = "Sip the wine and let it roll on the tongue. Note any flavors that may stand out.";
 	}
 }
 
