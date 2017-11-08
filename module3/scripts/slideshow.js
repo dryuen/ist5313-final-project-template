0149e3fa7d6fbbb9d53ba6856210eaf9e08a9e9a
wei <tdg054@my.utsa.edu>
2017-11-08 00:09:19 -0600

    slide show

---
+++ module3/scripts/slideshow.js
@@ -0,0 +1,21 @@
+var images = ["images/slide1.jpg", "images/slide2.jpg", "images/slide3.png", "images/slide4.jpg";
+ 
+ var heading = ["Take Advantage of Local Parks","GO Hiking","Ride a Bike","Push-up and Pull-ups","The Fourth Stage: Adult Butterfly"] ;
+ 
+ var imageNumber = 0;
+ var imageLength = images.length - 1;
+ 
+ function changeImage(x){
+	 imageNumber += x;
+	 if( imageNumber > imageLength ) {
+		 imageNumber = 0;
+	 }
+	 
+	 if (imageNumber < 0){
+		 imageNumber = imageLength;
+	 } 
+	 
+	 document.getElementById("slideshow").src = images[imageNumber];
+     document.getElementById("heading").innerHTML = heading[imageNumber];
+     return false;
+ }
