610905f8f5db1587418a3ede94d9458f9a407a06
sjill <sjill@DESKTOP-KJP8342>
2017-11-07 19:08:12 -0600

    fixed hotspot hints on mod1 ex2

--- module1/scripts/hot-spots.js
+++ module1/scripts/hot-spots.js
@@ -9,19 +9,23 @@ function showText( number )
 	
 	if( number == 1 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le chat";
+		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(0)-3 and y<sub>2</sub>=-4(0)+21";
 	}
 	else if( number == 2 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le cheval";
+		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(1)-3 and y<sub>2</sub>=-4(1)+21";
 	}
 	else if( number == 3 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "le chien";
+		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(2)-3 and y<sub>2</sub>=-4(2)+21";
 	}
 	else if( number == 4 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "l'oiseau";
+		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(3)-3 and y<sub>2</sub>=-4(3)+21";
+	}
+	else if( number == 5 )
+	{
+		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(4)-3 and y<sub>2</sub>=-4(4)+21";
 	}
 }
 
