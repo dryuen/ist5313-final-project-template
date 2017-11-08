c2c06791e1ad2d7d49afb88fe92cdd1931852e1a
wei <tdg054@my.utsa.edu>
2017-11-08 09:31:43 -0600

    slide show on emotional

--- module3/scripts/slideshow1.js
+++ module3/scripts/slideshow1.js
@@ -1,4 +1,4 @@
-var images = ["images/slide5.jpg", "images/slide6.jpg", "images/slide7.png"] ;
+var images = ["images/slide5.jpg", "images/slide6.jpg", "images/slide7.jpg"] ;
  var heading = ["Living With Less","Mentally Declutter","Slow Down Your Pace"] ;
  var caption = ["Better to donate than accumulate", " clear your mind of the obsessive thoughts,cut off the thought which cause overwhelm and keep you mired in
                     avoidance and indecision.", "  Slowing Down is Everything to Create Mental Peace.Focus on the lifestyle, not the goal. The journey is the destination.Enjoy the journey!"];
@@ -16,7 +16,8 @@ var images = ["images/slide5.jpg", "images/slide6.jpg", "images/slide7.png"] ;
 		 imageNumber = imageLength;
 	 } 
 	 
-	 document.getElementById("slideshow").src = images[imageNumber];
+	 document.getElementById("slideshow1").src = images[imageNumber];
      document.getElementById("heading").innerHTML = heading[imageNumber];
+     document.getElementById("caption").innerHTML = heading[imageNumber];
      return false;
  }
