ab9b04170add7cb22a4d179b31d686b4d746de95
Lee <Lee@192.168.0.14>
2017-11-15 20:30:05 -0600

    changed hot-spots.js to add explanation to ex2 mod 1

--- module1/scripts/hot-spots.js
+++ module1/scripts/hot-spots.js
@@ -9,23 +9,23 @@ function showText( number )
 	
 	if( number == 1 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(0)-3 and y<sub>2</sub>=-4(0)+21";
+		document.getElementById( "output-text" ).innerHTML = "Replace x with 0 so your equations look like this:<br><br> y<sub>1</sub>=2(0)-3 and y<sub>2</sub>=-4(0)+21";
 	}
 	else if( number == 2 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(1)-3 and y<sub>2</sub>=-4(1)+21";
+		document.getElementById( "output-text" ).innerHTML = "Replace x with 1 so your equations look like this:<br><br> y<sub>1</sub>=2(1)-3 and y<sub>2</sub>=-4(1)+21";
 	}
 	else if( number == 3 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(2)-3 and y<sub>2</sub>=-4(2)+21";
+		document.getElementById( "output-text" ).innerHTML = "Replace x with 2 so your equations look like this:<br><br> y<sub>1</sub>=2(2)-3 and y<sub>2</sub>=-4(2)+21";
 	}
 	else if( number == 4 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(3)-3 and y<sub>2</sub>=-4(3)+21";
+		document.getElementById( "output-text" ).innerHTML = "Replace x with 3 so your equations look like this:<br><br> y<sub>1</sub>=2(3)-3 and y<sub>2</sub>=-4(3)+21";
 	}
 	else if( number == 5 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "y<sub>1</sub>=2(4)-3 and y<sub>2</sub>=-4(4)+21";
+		document.getElementById( "output-text" ).innerHTML = "Replace x with 4 so your equations look like this:<br><br> y<sub>1</sub>=2(4)-3 and y<sub>2</sub>=-4(4)+21";
 	}
 }
 
