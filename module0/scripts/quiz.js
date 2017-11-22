01fbf7db1598d28a206e9ddfaabbd35c16e7b413
laurabenavidez <laurabenavidez@172.24.16.184>
2017-11-21 22:20:10 -0600

    edited quiz.js

--- module0/scripts/quiz.js
+++ module0/scripts/quiz.js
@@ -2,16 +2,16 @@
 
 function processQuiz()
 {
-	var answer 1 = docunment.forms.quiz1.question1. value; 
-	var answer 2 = docunment.forms.quiz1.question2. value; 
-	var answer 3 = docunment.forms.quiz1.question3. value; 
-	var answer 4 = docunment.forms.quiz1.question4. value; 
-	var answer 5 = docunment.forms.quiz1.question5. value; 
-	var answer 6 = docunment.forms.quiz1.question6. value; 
-	var answer 7 = docunment.forms.quiz1.question7. value; 
-	var answer 8 = docunment.forms.quiz1.question8. value; 
-	var answer 9 = docunment.forms.quiz1.question9. value; 
-	var answer 10 = docunment.forms.quiz1.question10. value; 
+	var answer1 = document.forms.quiz1.question1. value; 
+	var answer2 = document.forms.quiz1.question2. value; 
+	var answer3 = document.forms.quiz1.question3. value; 
+	var answer4 = document.forms.quiz1.question4. value; 
+	var answer5 = document.forms.quiz1.question5. value; 
+	var answer6 = document.forms.quiz1.question6. value; 
+	var answer7 = document.forms.quiz1.question7. value; 
+	var answer8 = document.forms.quiz1.question8. value; 
+	var answer9 = document.forms.quiz1.question9. value; 
+	var answer10 = document.forms.quiz1.question10. value; 
 	
 	var counter = 0;
 	
@@ -65,12 +65,15 @@ function processQuiz()
 		counter = counter + 1;
 	}
 	
-}
-
+alert ( "You got " + counter + " questions correct!");
+	
 var percentage = counter / 10;
+var grade = (percentage * 100);
+
+
 alert( "Your score is " + percentage + "%" );
 
-alert ( "You got " + counter + " questions correct!");
 
-sessionStorage.setItem( "somekey", "topic-7" );
-var value = sessionStorage.getItem( "topic-7" );
+
+
+}
