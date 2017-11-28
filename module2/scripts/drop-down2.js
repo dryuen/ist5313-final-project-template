120012dcd334686fb501128022310411ff034893
sjill <sjill@DESKTOP-KJP8342>
2017-11-27 22:02:41 -0600

    added solution drop downs to module 2

---
+++ module2/scripts/drop-down2.js
@@ -0,0 +1,20 @@
+function goFirst()
+{
+	if( document.getElementById( "xfirst" ).value == 2 )
+	{
+		
+		document.getElementById( "xfirst" ).disabled = "disabled";
+	}
+function goSecond()
+	{
+	if( document.getElementById( "xsecond" ).value == 1 )
+		{
+			
+		document.getElementById( "xsecond" ).disabled = "disabled";
+		}
+function goThird()
+		{
+			if( document.getElementById( "xthird" ).value == 3 )
+			{
+			document.getElementById( "xthird" ).disabled = "disabled";
+			}
