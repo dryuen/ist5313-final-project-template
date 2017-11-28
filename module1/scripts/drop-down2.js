bab18e441d0bec931fd9d81f0ef861be9eb3fefd
sjill <sjill@DESKTOP-KJP8342>
2017-11-27 21:53:06 -0600

    added drop down solutions to ex 3 and 4 in module 1

---
+++ module1/scripts/drop-down2.js
@@ -0,0 +1,20 @@
+function goFirst()
+{
+	if( document.getElementById( "xfirst" ).value == 3 )
+	{
+		
+		document.getElementById( "xfirst" ).disabled = "disabled";
+	}
+function goSecond()
+	{
+	if( document.getElementById( "xsecond" ).value == 3 )
+		{
+			
+		document.getElementById( "xsecond" ).disabled = "disabled";
+		}
+function goThird()
+		{
+			if( document.getElementById( "xthird" ).value == 1 )
+			{
+			document.getElementById( "xthird" ).disabled = "disabled";
+			}
--- module1/scripts/hot-spots2b.js
+++
@@ -1,9 +0,0 @@
-function goFirst()
-{
-	if( document.getElementById( "xfirst" ).value == 1 )
-	{
-		// enable the second <select>
-		document.getElementById( "xsecond" ).disabled = "";
-		// disable the first <select> (totally optional)
-		document.getElementById( "xfirst" ).disabled = "disabled";
-	}
