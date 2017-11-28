4b0009b7bcea6bcdb8ebc0abd0e531c6df0aa53b
sjill <sjill@DESKTOP-KJP8342>
2017-11-27 19:11:15 -0600

    added solution drop down menu to module 1 ex 2

--- module1/scripts/hot-spots.js
+++ module1/scripts/hot-spots.js
@@ -33,3 +33,13 @@ function hideOutput()
 {
 	document.getElementById( "output-box" ).style.display = "none";
 }
+
+function goFirst()
+{
+	if( document.getElementById( "xfirst" ).value == 1 )
+	{
+		// enable the second <select>
+		document.getElementById( "xsecond" ).disabled = "";
+		// disable the first <select> (totally optional)
+		document.getElementById( "xfirst" ).disabled = "disabled";
+	}
