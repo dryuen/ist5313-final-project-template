1cd825d4932de32a758430d974462c97e8d1ab77
Lee <Lee@192.168.0.14>
2017-12-04 15:19:45 -0600

    fixed scorm for mod 2 scripts.js and quiz2.js

--- module2/scripts/quiz2.js
+++ module2/scripts/quiz2.js
@@ -1,34 +1,33 @@
 function processQuiz()
-
+{
+	var answer1 = document.forms.quiz1.question1.value;
+	var answer2 = document.forms.quiz1.question2.value;
+	var answer3 = document.forms.quiz1.question3.value;
+	var answer4 = document.forms.quiz1.question4.value;
+		
+	var score = 0;
+			
+	if( answer1 == 3 )
 	{
-	
-	var answer1 = document.forms.quiz2.question1.value;
-	var answer2 = document.forms.quiz2.question2.value;
-	var answer3 = document.forms.quiz2.question3.value;
-	var answer4 = document.forms.quiz2.question4.value;
-	
-	var counter = 0;
-	
-	if( answer1 == 3)
-		{
-		counter = counter + 1;
-		}
-	
-	if( answer2 == 1)
-		{
-		counter = counter + 1;
-		}
-	
-	if( answer3 == 1)
-		{
-		counter = counter + 1;
-		}
-	
-	if( answer4 == 3)
-		{
-		counter = counter + 1;
-		}
-	
-	alert( "You got " + counter + " questions correct!")
-	
+		score = score + 25;
 	}
+			
+	if( answer2 == 1 )
+	{
+		score = score + 25;
+	}
+
+	if( answer3 == 1 )
+	{
+		score = score + 25;
+	}
+
+	if( answer4 == 3 )
+	{
+		score = score + 25;
+	}
+
+	alert( "You scored " + score )
+	parent.reportScores ( score );
+	
+}
--- module2/scripts/scripts.js
+++ module2/scripts/scripts.js
@@ -79,10 +79,16 @@ function initializeSCORM()
 
 // This function reports the score from the assessment to the LMS
 // This should only be called when the user submits the answers to the quiz
-function reportScores()
-{	
+function reportScores( score )
+{
+	oScorm.set("cmi.core.score.raw", score );
+	oScorm.set("cmi.core.score.min", 0 );
+	oScorm.set("cmi.core.score.max", 100 );
+	
+	// you need to check to see if score >= 70 to set this passed, otherwise it needs to be set to failed.
+	oScorm.set("cmi.core.lesson_status", "passed")
+	
 	oScorm.save();
-}
 
 // This function is called when the window is closed.  It saves and quits the course.
 function finishCourse()
