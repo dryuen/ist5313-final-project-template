0d1a8b61aa2de248a470591b77d84721f314689d
Lee <Lee@192.168.0.14>
2017-12-04 15:32:29 -0600

    fixeed coding on quiz 2 sand 3

--- module2/scripts/quiz2.js
+++ module2/scripts/quiz2.js
@@ -1,9 +1,9 @@
 function processQuiz()
 {
-	var answer1 = document.forms.quiz1.question1.value;
-	var answer2 = document.forms.quiz1.question2.value;
-	var answer3 = document.forms.quiz1.question3.value;
-	var answer4 = document.forms.quiz1.question4.value;
+	var answer1 = document.forms.quiz2.question1.value;
+	var answer2 = document.forms.quiz2.question2.value;
+	var answer3 = document.forms.quiz2.question3.value;
+	var answer4 = document.forms.quiz2.question4.value;
 		
 	var score = 0;
 			
