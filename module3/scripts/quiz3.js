87933dd885880a4213dc94c80585cb3cc2068299
sjill <sjill@192.168.0.17>
2017-12-06 18:34:06 -0600

    cert link appears for module 3 yeah!

--- module3/scripts/quiz3.js
+++ module3/scripts/quiz3.js
@@ -1,33 +1,41 @@
 function processQuiz()
-{
-	var answer1 = document.forms.quiz3.question1.value;
-	var answer2 = document.forms.quiz3.question2.value;
-	var answer3 = document.forms.quiz3.question3.value;
-	var answer4 = document.forms.quiz3.question4.value;
+	{
+		var answer1 = document.forms.quiz3.question1.value;
+		var answer2 = document.forms.quiz3.question2.value;
+		var answer3 = document.forms.quiz3.question3.value;
+		var answer4 = document.forms.quiz3.question4.value;
+			
+		var score = 0;
+				
+		if( answer1 == 1 )
+		{
+			score = score + 25;
+		}
+				
+		if( answer2 == 1 )
+		{
+			score = score + 25;
+		}
+
+		if( answer3 == 0 )
+		{
+			score = score + 25;
+		}
+
+		if( answer4 == 2 )
+		{
+			score = score + 25;
+		}
+
+		if( score >= 70)
+		{
 		
-	var score = 0;
-			
-	if( answer1 == 1 )
-	{
-		score = score + 25;
-	}
-			
-	if( answer2 == 1 )
-	{
-		score = score + 25;
-	}
+			document.getElementById( "certificate-link" ).style.display = "inline";
+		}
+		
+		alert( "You scored " + score )
+		parent.reportScores ( score );
 
-	if( answer3 == 0 )
-	{
-		score = score + 25;
+		
+		
 	}
-
-	if( answer4 == 2 )
-	{
-		score = score + 25;
-	}
-
-	alert( "You scored " + score )
-	parent.reportScores ( score );
-	
-}
