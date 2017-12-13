3e1cde21e69a4d59d746a8609b78ef647799cb56
Vivian <vcnales@gmail.com>
2017-12-12 21:13:10 -0600

    module 3 updated with modal, certificate, and student name

--- module3/scripts/scripts.js
+++ module3/scripts/scripts.js
@@ -88,8 +88,6 @@ function reportScores(score)
     oScorm.set("cmi.core.score.min", 0);
     oScorm.set("cmi.core.score.max", 100);
 
-    alert( "Reported " + score + " as your score." );
-
     if(score > 70){
         oScorm.set("cmi.core.lesson_status", "passed");
     } else {
@@ -101,8 +99,8 @@ function reportScores(score)
 }
 
 function viewCertificate(){
-    var learnerName = oScorm.get("cmi.core.student_name");
-    document.getElementById("content-frame").contentWindow.document.getElementById("user-name").innerHTML = learnerName;
+    var studentName = oScorm.get("cmi.core.student_name");
+    document.getElementById("content-frame").contentWindow.document.getElementById("student-name").innerHTML = studentName;
 }
 
 // This function is called when the window is closed.  It saves and quits the course.
@@ -181,7 +179,6 @@ function checkAllVisited(){
 		t8 == "visited" && t9 == "visited"){
 
         document.getElementById("content-frame").contentWindow.document.getElementById("quiz-link").style.display="block";
-        // document.getElementById("content-frame").contentWindow.document.getElementById("quiz-button").style.display="block";
 
     }
 }
